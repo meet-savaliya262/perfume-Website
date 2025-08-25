d89c3fc30d2e04cbe246b5da16f583a26e6b0108
meet-savaliya262 <meetsavliya13@gmail.com>
2025-08-25 18:15:39 +0530

    modifie index and header page

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -35,7 +35,7 @@
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
       <li class="nav-item"><a class="nav-link" data-widget="navbar-search" href="#" role="button">Hi,<?php echo $_SESSION['admin']['email']; ?></a></li>
-      <li class="nav-item"><a class="nav-link" data-widget="navbar-search" href="logout.php" role="button">Log Out</a></li>
+      <li class="nav-item"><a class="nav-link" href="logout.php" role="button"><i class="fas fa-sign-out-alt"></i>&nbsp;Log Out</a></li>
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -43,7 +43,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Admin panel</span>
     </a>
